cf16c15998370efcccd4e60c9b548470f34ac28a
William Haesemeyer <willhaese@gmail.com>
2017-06-01 20:36:48 -0400

    added coin collection

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,4 +18,13 @@ public class PlayerController : MonoBehaviour {
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         rb.AddForce (movement * speed);
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+        }
+        
+    }
 }
+
