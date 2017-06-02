900b92b6cb0fbee62f93f61adae3d2c17cc57326
William Haesemeyer <willhaese@gmail.com>
2017-06-01 20:51:00 -0400

    almost done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
+    public Text countText;
     public float speed;
 
     private Rigidbody rb;
+    private int count;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-    }
+        count = 0;
+        SetCountText();
+ }
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
@@ -23,8 +29,14 @@ public class PlayerController : MonoBehaviour {
         if(other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
         }
         
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
     }
 }
 
